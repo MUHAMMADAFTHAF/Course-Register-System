53e02913a9ef08f1f9d60ae29a7da4da30446b1d
MUHAMMAD AFTHAF <ma.afthaf@gmail.com>
2026-06-10 20:04:57 +0530

    Add files via upload
    front end

---
+++ cscript.js
@@ -0,0 +1,37 @@
+function showCourses(){
+    fetch("http://localhost:8080/courses")
+    .then((response) => response.json())
+    .then((courses) => {
+        const dataTable = document.getElementById("courseTable")
+
+        courses.forEach(course =>{
+            var row = `<tr>
+            <td>${course.courseId }</td>
+             <td>${course.courseName }</td>  
+             <td>${course.trainer }</td>
+             <td>${course.durationInWeeks }</td>
+            </tr>`
+
+            dataTable.innerHTML+=row;
+        })
+    })
+}
+
+
+function showEnrolled(){
+    fetch("http://localhost:8080/courses/enrolled")
+    .then((response) => response.json())
+    .then((student) => {
+        const dataTable = document.getElementById("enrollTable")
+
+        student.forEach(student =>{
+            var row = `<tr>
+            <td>${student.name }</td>
+             <td>${student.emailId }</td>  
+             <td>${student.courseName }</td>
+            </tr>`
+
+            dataTable.innerHTML+=row;
+        })
+    })
+}
